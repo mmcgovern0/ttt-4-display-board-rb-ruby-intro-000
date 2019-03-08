1ce2251b1573ceb63fca898c1045d050748446ad
Michael McGovern <mmcgovern.wework@gmail.com>
2019-03-08 21:27:40 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
-board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
-
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
